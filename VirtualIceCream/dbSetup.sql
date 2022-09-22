d562c092a496ccf5d2b735b4ad807c7b871563e6
Curtis Pollard <clp.the.second@gmail.com>
2022-09-21 19:03:52 -0600

    Wednesday Commit

--- VirtualIceCream/dbSetup.sql
+++ VirtualIceCream/dbSetup.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS accounts(
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
 
+SELECT * FROM accounts
+
 
 -- Active: 1663174271535@@SG-SQL-FIrst-Try-6712-mysql-master.servers.mongodirector.com@3306@firstTry
 -- Orders
@@ -41,7 +43,50 @@ CREATE TABLE Ingredients(
     FOREIGN KEY (creatorId) REFERENCES accounts(id)
 ) DEFAULT charset utf8;
 
+ SELECT
+i.*,
+a.*
+FROM Ingredients i
+JOIN accounts a ON a.id = i.creatorId;
+
 INSERT INTO `Ingredients`
 (name, price, image, creatorId)
 VALUES
 ("Reese's", 2, "http://throughherlookingglass.com/wp-content/uploads/2016/09/Chocolate-Peanut-Butter-Trifle6.jpg", "6328c5a5f170ebe2ab203b70");
+
+
+-- Favorties
+
+CREATE TABLE IF NOT EXISTS favorities(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  orderId INT NOT NULL,
+  accountId VARCHAR(255),
+
+  FOREIGN KEY (orderId) REFERENCES Orders(id) ON DELETE CASCADE,
+  FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+
+SELECT 
+  o.*,
+  a.*
+FROM Orders o 
+JOIN accounts a On a.id = o.creatorId
+Where o.id = 3;
+
+INSERT INTO favorities
+(orderId, accountId)
+VALUES
+(3, '6328c5a5f170ebe2ab203b70');
+
+SELECT 
+a.*,
+f.*
+FROM favorities f
+JOIN accounts a ON a.id = f.accountId
+WHERE f.orderId = 3;
+
+ INSERT INTO favorites
+(orderId, accountId)
+VALUES
+(@orderId, @accountId);
